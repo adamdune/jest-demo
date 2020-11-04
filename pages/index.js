5c115f86824924286f0da2ae1261b70e70d35a44
adamho <adam.ho@growthops.asia>
2020-11-04 13:30:01 +0800

    undome

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,64 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import { useState } from "react"
 
 export default function Home() {
+  const [todos, setTodos] = useState([
+    { title: "do stuff and things", isDone: false },
+    { title: "something else", isDone: true },
+  ])
+
+  const [newTodo, setNewTodo] = useState("")
+
+  const onChangeCheckbox = (e, i) => {
+    setTodos(
+      todos.map((todo, j) => {
+        if (i === j) {
+          return { ...todo, isDone: e.target.checked }
+        }
+        return todo
+      }),
+    )
+  }
+
+  const createTodo = () => {
+    if (newTodo) {
+      setTodos(todos.concat([{ title: newTodo, isDone: false }]))
+      setNewTodo("")
+    }
+  }
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Todo App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main>
+        <div>
+          <h1>Todo App</h1>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button onClick={createTodo}>Add New Todo</button>
+          <div>
+            {todos.map((todo, i) => (
+              <div key={i}>
+                <input
+                  type="checkbox"
+                  onChange={(e) => onChangeCheckbox(e, i)}
+                  checked={todo.isDone}
+                />
+                <p className={todo.isDone ? `line-through` : "bg-red-500"}>
+                  {todo.title}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
